2de6caa441fb2910bda8c57cdb8ac2dc0bf1410d
Chocsheep <gabi.novak21@gmail.com>
2026-05-19 12:06:29 +1000

    removing flask server conflict

--- app.py
+++ app.py
@@ -1,35 +1,6 @@
-from flask import Flask, render_template, request, redirect
-import sqlite3
-from datetime import datetime
-import random
+from app import app
 import os
- 
-app = Flask(__name__)
-DATABASE = os.path.join(os.path.dirname(__file__), 'database1.db')
- 
-def get_db_connection():
-    conn = sqlite3.connect(DATABASE)
-    conn.row_factory = sqlite3.Row
-    return conn
 
-# home page -> essentially what is first loaded when a user logs in to the website 
-@app.route("/")
-def home():
-    conn = get_db_connection()
-    conn.close()
-    return render_template("home.html")
-
-@app.route("/testing")
-def test():
-    return render_template("login.html")
- 
 if __name__ == '__main__':
-    conn = get_db_connection()
-    conn.close()
-
-    # Railway injects its own port via environment variable
     port = int(os.environ.get("PORT", 5000))
-
-    # lets Railway's network reach the app
     app.run(debug=False, host="0.0.0.0", port=port)
- 
